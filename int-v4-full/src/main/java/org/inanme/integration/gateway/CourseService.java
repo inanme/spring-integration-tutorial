4413d0f6e298f71d0753768a88a2a85cb4cf95b3
mert inan <inanme@gmail.com>
2016-05-07 14:59:27 +0100

    removed garbage

--- int-v4-full/src/main/java/org/inanme/integration/gateway/CourseService.java
+++
@@ -1,9 +0,0 @@
-package org.inanme.integration.gateway;
-
-import org.inanme.integration.Channels;
-import org.springframework.integration.annotation.MessagingGateway;
-
-@MessagingGateway(defaultRequestChannel = Channels.REQUEST_CHANNEL)
-public interface CourseService {
-    String findCourse(String courseId);
-}
